ad5ad7fd1a540d34dd5580d92324de63bc5ec79a
Tom Bogman <t.bogman@mosadex.nl>
2018-11-13 09:21:03 +0100

    layout

--- resources/views/Home/index.blade.php
+++ resources/views/Home/index.blade.php
@@ -24,31 +24,53 @@
     </div>
     <div class="row">
         {{-- @foreach($products as $product) --}}
-        <div class="col-3 text-center image">
-            image
+        <div class="col-3 products">
+            <div class="row">
+                <div class="col-12 text-center image">
+                    image
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 text-center subtext">
+                    Test 1
+                </div>
+            </div>
         </div>
-        <div class="col-3 text-center image2">
-            image
+        <div class="col-3 products">
+            <div class="row">
+                <div class="col-12 text-center image">
+                    image
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 text-center subtext">
+                    Test 1
+                </div>
+            </div>
         </div>
-        <div class="col-3 text-center image">
-            image
+        <div class="col-3 products">
+            <div class="row">
+                <div class="col-12 text-center image">
+                    image
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 text-center subtext">
+                    Test 1
+                </div>
+            </div>
         </div>
-        <div class="col-3 text-center image3">
-            image
-        </div>
-
-        <div class="col-3 text-center subtext">
-            Test 1
-        </div>
-        {{-- @endforeach --}}
-        <div class="col-3 text-center subtext2">
-            Test 2
-        </div>
-        <div class="col-3 text-center subtext">
-            Test 3
-        </div>
-        <div class="col-3 text-center subtext3">
-            Test 4
+        <div class="col-3 products">
+            <div class="row">
+                <div class="col-12 text-center image">
+                    image
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 text-center subtext">
+                    Test 1
+                </div>
+            </div>
         </div>
     </div>
     <div class="row">
@@ -63,6 +85,9 @@
         <div class="col-6 text-center graph2">super text</div>
     </div>
 </div>
+
+
+
 
 
 @stop
